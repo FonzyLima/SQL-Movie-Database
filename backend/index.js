776dcc4d6526fdcc5cac8be0a1fbd03ad4861027
FonzyLima <71457986+FonzyLima@users.noreply.github.com>
2022-02-13 03:35:14 +0800

    initialized database

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const mysql = require('mysql');
+
 const app = express();
+const db = mysql.createConnection({
+    host: process.env.HOSTNAME,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: 3306
+})
 
 app.listen(3001, () =>{
     console.log("Running on port 3001");
