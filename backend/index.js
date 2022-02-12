8b141bdb2c0133499dc1044cdd3fdaead87efdee
FonzyLima <71457986+FonzyLima@users.noreply.github.com>
2022-02-13 03:44:06 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 
 const app = express();
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.HOSTNAME,
     user: process.env.USER,
     password: process.env.PASSWORD,
