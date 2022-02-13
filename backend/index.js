c30f8c9d6dd99ca680fe85dccd2537cf3d03f5c9
FonzyLima <71457986+FonzyLima@users.noreply.github.com>
2022-02-13 20:30:35 +0800

    update

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,13 @@ const db = mysql.createPool({
     database: process.env.DATABASE,
     port: 3306
 })
-
+app.get('/', (req,res)=>{
+    const sqlRead = "SELECT * FROM mco2.movies LIMIT 1";
+    db.query(sqlRead,(err,result)=>{
+        res.send(result)
+        
+    })
+})
 app.listen(3001, () =>{
     console.log("Running on port 3001");
 })
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 function App() {
   return (
     <div className="page">
@@ -7,6 +7,31 @@ function App() {
           <span className="section">STADVDB S14</span>
           <span className="names">Berenguer, Camarillo, Lima</span>
         </div>
+      </div>
+      <div className="body">
+        <table
+          id="dtBasicExample"
+          className="table table-striped table-bordered table-sm"
+          cellspacing="0"
+          width="100%"
+        >
+          <thead>
+            <tr>
+              <th className="th-sm">ID</th>
+              <th className="th-sm">Name</th>
+              <th className="th-sm">Year</th>
+              <th className="th-sm">Rank</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tiger Nixon</td>
+              <td>System Architect</td>
+              <td>Edinburgh</td>
+              <td>61</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
