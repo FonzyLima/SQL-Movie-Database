877229e9647a21088543be1fb47ae92ce7a8b6ea
camarillobryanb <bryan_daniel_camarillo@dlsu.edu.ph>
2022-02-13 23:17:30 +0800

    modal

---
+++ frontend/src/component/Create.js
@@ -0,0 +1,32 @@
+import React, {Fragment} from 'react';
+
+const Create = () => {
+    return (
+        <Fragment>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target={`#create`}>Create</button>
+            <div id='create' class="modal fade" role="dialog">
+                <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                    <h4 class="modal-title">Create</h4>
+                    </div>
+                    <div class="modal-body">
+                        <a>Name</a>
+                        <input type="text" className="form-control"/>
+                        <a>Year</a>
+                        <input type="text" className="form-control"/>
+                        <a>Rank</a>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary" data-dismiss="modal">Create</button>
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </Fragment>
+    );
+}
+
+export default Create;
