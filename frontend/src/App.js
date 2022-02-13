254a5f96b06642d1cbc1f93283b74bcb2138026e
FonzyLima <71457986+FonzyLima@users.noreply.github.com>
2022-02-13 20:03:45 +0800

    header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
+import './App.css';
 function App() {
   return (
-    <div>
-      
+    <div className="page">
+      <div className="header">
+        <div className="headerContent">
+          <span className="section">STADVDB S14</span>
+          <span className="names">Berenguer, Camarillo, Lima</span>
+        </div>
+      </div>
     </div>
   );
 }
