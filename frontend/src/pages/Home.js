f07b043b0c918bf25a5af8eb5b99a9c903f06916
FonzyLima <71457986+FonzyLima@users.noreply.github.com>
2022-02-14 23:23:43 +0800

    fixed router

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,74 @@
+import "../styles/home.css";
+import React, { useEffect, useState } from "react";
+import Navbar from "../component/Navbar";
+import Create from "../component/Create";
+import Update from "../component/Update";
+import Delete from "../component/Delete";
+const axios = require("axios");
+
+const Home = () =>{
+    const [data, setData] = useState([]);
+  const getData = async () => {
+    try {
+      const getMovieData = await axios({
+        url: "http://localhost:3001/readAll",
+        method: "get",
+      });
+      setData(getMovieData.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  console.log("HEY");
+  console.log(data);
+    return(
+        <div className="page">
+            <Navbar />
+            <div className="title">
+              <h3>MCO2: Transaction Management</h3>
+            </div>
+            <Create />
+            <div className="body">
+              <table
+                id="dtBasicExample"
+                className="tableproper"
+                // cellpadding="10px"
+                // cellspacing="0"
+                // width="100%"
+              >
+                <thead>
+                  <tr>
+                    <th className="tablecell">ID</th>
+                    <th className="tablecell">Name</th>
+                    <th className="tablecell">Year</th>
+                    <th className="tablecell">Rank</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {data.map((item) => (
+                    <tr key={item.id}>
+                      <td>{item.id}</td>
+                      <td>{item.name}</td>
+                      <td>{item.year}</td>
+                      <td>{item.rank}</td>
+                      <td>
+                        <Update />
+                      </td>
+                      <td>
+                        {" "}
+                        <Delete />
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+    )
+}
+
+export default Home;
